b44c6d5112ddfe30788bab5031aa11044ba5660a
robertka <robertka@infosupport.com>
2018-11-15 11:35:20 +0100

    gitignore fix to include configs

---
+++ 02-User-Profile/src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: 'docati.eu.auth0.com',
+  clientId: 'J5iMjj2Do0Ap58YiPbx73MmD70h7r5hq',
+  callbackUrl: 'http://localhost:3000/callback',
+  logoutRedirectUrl: 'http://localhost:3000',
+}
